67b7a69d97bc5d68179c46cc829606c2dbbfad79
lekhuong1906 <lekhuong190602@gmail.com>
2024-05-13 20:38:07 +0700

    fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
-            '/',
+            '/post',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
